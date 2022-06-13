e3b7968420384531265372818bd8f796f53dd833
Chenqin Zhang <Renyqin@users.noreply.github.com>
2022-06-13 18:59:02 +0800

    Time: 81 ms (32.32%), Space: 14.5 MB (57.02%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -8,20 +8,25 @@ class Solution:
     """
     
     def isPalindrome(self, s: str) -> bool:
-        cleaned_s = []
-        for l in s:
-            if l.isalpha() or l.isdigit():
-                cleaned_s.append(l.lower())
         
         i = 0
-        j = len(cleaned_s)-1
+        j = len(s)-1
         while i<j:
-            if cleaned_s[i] != cleaned_s[j]:
+            
+            if not (s[i].isalpha() or s[i].isdigit()):
+                i+=1
+                continue
+            elif not (s[j].isalpha() or s[j].isdigit()):
+                j-=1
+                continue
+                
+            elif s[i].lower() != s[j].lower():
                 return False
             
+
             i += 1
             j -= 1
-        
+
         return True
             
             
