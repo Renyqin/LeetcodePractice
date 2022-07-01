1adcc83b531e208201fe8f054741711b3cccb5e3
Chenqin Zhang <Renyqin@users.noreply.github.com>
2022-07-01 21:41:48 +0800

    Time: 48 ms (48.74%), Space: 14 MB (23.80%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,24 +1,3 @@
-"""class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        for prth in s:
-            if len(stack) > 0:
-                if (prth == "(" and stack[-1] == ")") or \
-                    (prth == ")" and stack[-1] == "(") or\
-                    (prth == "[" and stack[-1] == "]") or\
-                    (prth == "]" and stack[-1] == "[") or\
-                    (prth == "{" and stack[-1] == "}") or\
-                    (prth == "}" and stack[-1] == "{"):
-                        stack.pop()
-                        
-                else:
-                    stack.append(prth)
-            else:
-                stack.append(prth)
-                
-        return len(stack) == 0"""
-    
-
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
