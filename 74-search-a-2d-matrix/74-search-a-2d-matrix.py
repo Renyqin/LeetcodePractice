0cc380f3988a1e0beda48424220cdd399f28960a
Chenqin Zhang <Renyqin@users.noreply.github.com>
2022-07-29 22:20:37 +0800

    Time: 80 ms (28.86%), Space: 14.4 MB (42.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,21 +1,30 @@
 class Solution:
+    
+    
+    def indexToFlat(self, index, row_len):
+        return index[0]*row_len + index[1]
+    
+    def flatToIndex(self, flat, row_len):
+        return [flat//row_len, flat%row_len] 
+    
+    
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        flat_rows = []
-        for row in matrix:
-            flat_rows += row
         
-        # print(flat_rows)
-        left = 0
-        right = len(flat_rows)-1
-        while right >= left:
-            mid = left + (right-left)//2
-            #print(mid)
-            if target == flat_rows[mid]:
+        row_len = len(matrix[0])
+        left_flat = self.indexToFlat([0,0], row_len)
+        right_flat = self.indexToFlat([len(matrix)-1, len(matrix[0])-1], row_len)
+        
+        while left_flat <= right_flat:
+            mid_flat = (left_flat+right_flat) //2
+            mid = self.flatToIndex(mid_flat, row_len)
+            mid_value = matrix[mid[0]][mid[1]]
+            if target == mid_value:
                 return True
-            elif target > flat_rows[mid]:
-                left = mid + 1
+            elif target < mid_value:
+                right_flat = mid_flat - 1
             else:
-                right = mid - 1
+                left_flat = mid_flat + 1
+    
         
         return False
     
