8b0bdd1131b00c44dc39e1ef54e5204c3683b62c
Chenqin Zhang <Renyqin@users.noreply.github.com>
2022-08-03 16:21:08 +0800

    Time: 59 ms (71.73%), Space: 17.2 MB (14.84%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -5,22 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.prev = float("-inf")
+        self.ans = True
     
-    def helper(self, root, val_list):
+    def helper(self, root):
 
         if not root:
             return 
-        self.helper(root.left, val_list)
-        val_list.append(root.val)
-        self.helper(root.right, val_list)
+        self.helper(root.left)
+        if root.val <= self.prev:
+            self.ans = False
+            return 
+        self.prev = root.val
+        self.helper(root.right)
         
         
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        val_list = []
-        self.helper(root, val_list)
-        #print(val_list)
-        for i in range(len(val_list)-1):
-            if val_list[i+1] <= val_list[i] :
-                return False
+        self.helper(root)
+    
             
-        return True
+        return self.ans
