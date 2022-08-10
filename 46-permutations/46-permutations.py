88624f8a19ec8ca6e75429c60a8445654c03e33c
Chenqin Zhang <Renyqin@users.noreply.github.com>
2022-08-10 08:42:43 +0800

    Time: 63 ms (50.91%), Space: 14.1 MB (58.61%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,18 +1,19 @@
 class Solution:
     
-    def helper(self, nums, path, ans, visited):
+    def helper(self, nums, path, ans):
+        #print(path)
         if len(path) == len(nums):
-            ans.append(path)
-        
+            ans.append(path.copy())
         
         for i in range(len(nums)):
-            if i not in visited:
-                visited.add(i)
-                self.helper(nums, path+[nums[i]], ans, visited)
-                visited.remove(i)
+            if nums[i] not in path:
+                path.append(nums[i])
+                self.helper(nums, path, ans)
+                path.remove(nums[i])
     
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
         visited = set()
-        self.helper(nums, [], ans, visited)
+        path = []
+        self.helper(nums, path, ans)
         return ans
