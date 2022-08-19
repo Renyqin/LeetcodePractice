141bc13db592d8f1ffa4edf9f9932f09b501286c
Chenqin Zhang <Renyqin@users.noreply.github.com>
2022-08-19 14:13:04 +0800

    Time: 7007 ms (9.17%), Space: 21.8 MB (12.10%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -7,24 +7,17 @@ class Solution:
         for i in range(len(s)):
             dp[i][i] = True
             
+        max_len = 0
+        max_string = s[0]
+            
         for right in range(len(s)):
             for left in range(right):
                 #print(str(j) + " " + str(i))
                 dp[left][right] = (s[left] == s[right]) and (right-left < 3 or dp[left+1][right-1])
-        
-        
-        #print(dp)
-        
-      
-        max_len = 0
-        max_string = s[0]
-        for right in range(len(s)-1, -1, -1):
-            for left in range(right-1, -1, -1):
                 if dp[left][right] and (right-left + 1) > max_len:
                     max_len = right-left + 1
                     max_string = s[left:right+1]
-            
         
-            
+        
         return max_string
         
