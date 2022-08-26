14fd548498fa796e31bf97403f769f2e2a18efa6
Chenqin Zhang <Renyqin@users.noreply.github.com>
2022-08-26 13:58:57 +0800

    Time: 149 ms (34.63%), Space: 15.7 MB (7.06%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        dp = [nums[0]]
+        neg_dp = [nums[0]]
+        for i in range(1, len(nums)):
+            maximum = max([nums[i] * dp[i-1], nums[i] * neg_dp[i-1], nums[i]]) 
+            minimum = min([nums[i] * dp[i-1], nums[i] * neg_dp[i-1], nums[i]]) 
+            dp.append(maximum)
+            neg_dp.append(minimum)
+            
+                
+ 
+        return max(max(dp), max(neg_dp))
